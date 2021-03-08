a045d014836db4a0cce509477afd1540c9e6abb9
meng <mengzhou44@gmail.com>
2021-03-08 07:04:50 -0500

    commit

--- src/components/home/banner.tsx
+++ src/components/home/banner.tsx
@@ -1,238 +1,264 @@
-import  {useState} from 'react'
+import { useState } from 'react'
 import styled from 'styled-components'
 
-
 enum Tab {
-   SaveWith, 
-   GlobalETFPortfolios,
-   GreatMortgageRates
+	SaveWith,
+	GlobalETFPortfolios,
+	GreatMortgageRates,
 }
 
 const BannerBox = styled.div`
-      
-      height: 41.5rem; 
-      display: flex; 
-      flex-direction: column;
-      justify-content: space-between;
-      border-bottom: 1px solid var(--grey-1);
+	height: 41.5rem;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	border-bottom: 1px solid var(--grey-1);
 
-      .slide-box {
-        flex: 1;         
-        position: relative;
-         
-        .nav {
-            position: absolute; 
-            border-radius: 50%;
-            background-color: #fff; 
-            width: 3rem;
-            height: 3rem; 
-            display: flex;
-            justify-content:center;
-            align-items:center; 
-            background-color: var(--tab-color);
-          
-            cursor: pointer; 
-            transition: all .2s;
-            
-            &:hover {
-                transform: translateY(-2px);
-                box-shadow: var(--shadow-light);
-            }
+	.slide-box {
+		flex: 1;
+		position: relative;
 
-            i {                
-                display: flex;
-                justify-content:center;
-                align-items:center; 
-                font-size: 2.5rem;
-                color: var(--grey-1);
-            }
-        }
+		.nav {
+			position: absolute;
+			border-radius: 50%;
+			background-color: #fff;
+			width: 3rem;
+			height: 3rem;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			background-color: var(--tab-color);
 
-        .nav-prev{
-                top: 43%;
-                left: 1rem; 
-         }
+			cursor: pointer;
+			transition: all 0.2s;
 
-        .nav-next{
-                top: 43%;
-                right: 1rem; 
-         }
+			&:hover {
+				transform: translateY(-2px);
+				box-shadow: var(--shadow-light);
+			}
 
-           
-        
-      }
+			i {
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				font-size: 2.5rem;
+				color: var(--grey-1);
+			}
+		}
 
-      .slide {
-         height:100%;
-         width: 100%;
-         background-color: var(--text-color);
-         background-size:cover; 
-      }
+		.nav-prev {
+			top: 43%;
+			left: 1rem;
+		}
 
-      .slide.slide-saveWith {
-         background-image: url('images/ETF_launch_tablet.jpeg');
-         animation: moveToRight .5s;
-      }
-     
-      .slide.slide-globalETFPortfolios {
-         background-image: url('images/bundle_tablet.jpeg');
-         animation: moveToRight .5s;
-      }
+		.nav-next {
+			top: 43%;
+			right: 1rem;
+		}
+	}
 
-      .slide.slide-greatMortgageRates {
-        background-image: url('images/mtg-awareness-tablet.jpeg');
-        animation: moveToRight .5s;
-      }
+	.slide {
+		height: 100%;
+		width: 100%;
+		background-color: var(--text-color);
+		background-size: cover;
+		transform: translateX(-100%);
+		transition: all 1s;
+	}
 
-     .tabs {
-         height: 7.6rem;
-         width:100%;
-         background-color: #fff;
+	.slide.slide-saveWith {
+		background-image: url('images/ETF_launch_tablet.jpeg');
+		transform: translateX(0);
+	}
 
-         list-style: none;
-         display: flex;
-         font-size: 19px;
-         color: var(--tab-color);
-         font-weight: 700; 
-        
-         li {
-             cursor: pointer; 
-             flex: 1; 
-             display: flex; 
-             justify-content: center; 
-             align-items: center; 
+	.slide.slide-globalETFPortfolios {
+		background-image: url('images/bundle_tablet.jpeg');
+		transform: translateX(0);
+	}
 
-             margin: 1.5rem 0;
+	.slide.slide-greatMortgageRates {
+		background-image: url('images/mtg-awareness-tablet.jpeg');
+		transform: translateX(0);
+	}
 
-             &:not(:last-of-type) {
-                 border-right: 1px solid var(--tab-color);
-             }
+	.tabs {
+		height: 7.6rem;
+		width: 100%;
+		background-color: #fff;
 
-             &.selected {
-                 border-right: none;
-                 background-color: var(--primary);
-                 color: #fff;
-                 margin: 0;
-                 position: relative; 
-             }
+		list-style: none;
+		display: flex;
+		font-size: 19px;
+		color: var(--tab-color);
+		font-weight: 700;
 
-             &.selected::after {
-                width: 30px;
-                height: 30px;
-                content: '';
-                transform: translateX(-50%) rotate(45deg);
-                border-radius: 6px;
-                background-color: var(--primary);
-                position: absolute;
-                z-index: 0;
-                left: 50%;
-                top: -11px;
-             }
-         }
-     }
+		li {
+			cursor: pointer;
+			flex: 1;
+			display: flex;
+			justify-content: center;
+			align-items: center;
 
-     .globalETFPortfolios .saveWith {
-                border-right: none !important;
-      }
+			margin: 1.5rem 0;
 
-     .greatMortgageRates .globalETFPortfolios {
-               border-right: none !important;
-           
-     }
+			&:not(:last-of-type) {
+				border-right: 1px solid var(--tab-color);
+			}
 
+			&.selected {
+				border-right: none;
+				background-color: var(--primary);
+				color: #fff;
+				margin: 0;
+				position: relative;
+			}
+
+			&.selected::after {
+				width: 30px;
+				height: 30px;
+				content: '';
+				transform: translateX(-50%) rotate(45deg);
+				border-radius: 6px;
+				background-color: var(--primary);
+				position: absolute;
+				z-index: 0;
+				left: 50%;
+				top: -11px;
+			}
+		}
+	}
+
+	.globalETFPortfolios .saveWith {
+		border-right: none !important;
+	}
+
+	.greatMortgageRates .globalETFPortfolios {
+		border-right: none !important;
+	}
 `
 
-function getTabName(tab:Tab) {
-    switch(tab) {
-        case Tab.SaveWith:
-             return 'saveWith'
-        case Tab.GlobalETFPortfolios:
-             return  'globalETFPortfolios'
-        default: 
-             return 'greatMortgageRates'
-    }
+function getTabName(tab: Tab) {
+	switch (tab) {
+		case Tab.SaveWith:
+			return 'saveWith'
+		case Tab.GlobalETFPortfolios:
+			return 'globalETFPortfolios'
+		default:
+			return 'greatMortgageRates'
+	}
 }
 
 export default function Banner() {
-    const [currentTab, setCurrentTab] = useState(Tab.SaveWith)
+	const [currentTab, setCurrentTab] = useState(Tab.SaveWith)
 
-    function getTabsClass() {
-         return `tabs ${getTabName(currentTab)}`
-    }
+	function getTabsClass() {
+		return `tabs ${getTabName(currentTab)}`
+	}
 
-    function getTabClass(tab:Tab) {
-        let res= getTabName(tab)
-        if (tab === currentTab) return   `${res} selected`        
-        return res;
-    }
+	function getTabClass(tab: Tab) {
+		let res = getTabName(tab)
+		if (tab === currentTab) return `${res} selected`
+		return res
+	}
 
-    function renderSlide() {
-        switch(currentTab) {
-             case Tab.SaveWith: 
-                  return  <div className='slide slide-saveWith'>
-                  </div>
-             case Tab.GlobalETFPortfolios:
-                  return  <div className='slide slide-globalETFPortfolios'>
-                </div>   
-             default:
-                  return  <div className='slide slide-greatMortgageRates'>
-                 </div>    
-        }
-    }
+	function renderSlide() {
+		switch (currentTab) {
+			case Tab.SaveWith:
+				return (
+					<div className='slide slide-saveWith'></div>
+				)
+			case Tab.GlobalETFPortfolios:
+				return (
+					<div className='slide slide-globalETFPortfolios'></div>
+				)
+			default:
+				return (
+					<div className='slide slide-greatMortgageRates'></div>
+				)
+		}
+	}
 
-    function moveToPrev() {
-        switch(currentTab) {
-            case Tab.SaveWith: 
-                 setCurrentTab(Tab.GreatMortgageRates)
-                 break
-            case Tab.GlobalETFPortfolios:
-                setCurrentTab(Tab.SaveWith)
-                break
-            default:
-                setCurrentTab(Tab.GlobalETFPortfolios)
-        }
-    }
+	function moveToPrev() {
+		switch (currentTab) {
+			case Tab.SaveWith:
+				setCurrentTab(Tab.GreatMortgageRates)
+				break
+			case Tab.GlobalETFPortfolios:
+				setCurrentTab(Tab.SaveWith)
+				break
+			default:
+				setCurrentTab(Tab.GlobalETFPortfolios)
+		}
+	}
 
-    function moveToNext() {
-        switch(currentTab) {
-            case Tab.SaveWith: 
-                 setCurrentTab(Tab.GlobalETFPortfolios)
-                 break
-            case Tab.GlobalETFPortfolios:
-                setCurrentTab(Tab.GreatMortgageRates)
-                break
-            default:
-                setCurrentTab(Tab.SaveWith)
-        }
-    }
+	function moveToNext() {
+		switch (currentTab) {
+			case Tab.SaveWith:
+				setCurrentTab(Tab.GlobalETFPortfolios)
+				break
+			case Tab.GlobalETFPortfolios:
+				setCurrentTab(Tab.GreatMortgageRates)
+				break
+			default:
+				setCurrentTab(Tab.SaveWith)
+		}
+	}
 
-    return  <BannerBox>        
-            <div className='slide-box'>
-             {renderSlide()} 
+	return (
+		<BannerBox>
+			<div className='slide-box'>
+				{renderSlide()}
 
-            <div className='nav nav-prev'   aria-label='Previous' onClick={()=> moveToPrev()}><i className='icon-leftarrow'></i></div>
-            <div className='nav nav-next'  aria-label='Next' onClick={()=> moveToNext()} ><i className='icon-rightarrow'></i></div>
+				<div
+					className='nav nav-prev'
+					aria-label='Previous'
+					onClick={() => moveToPrev()}
+				>
+					<i className='icon-leftarrow'></i>
+				</div>
+				<div
+					className='nav nav-next'
+					aria-label='Next'
+					onClick={() => moveToNext()}
+				>
+					<i className='icon-rightarrow'></i>
+				</div>
+			</div>
 
-            </div>  
-          
-            <ul className={getTabsClass()}>
-                  <li 
-                      onClick={()=> setCurrentTab(Tab.SaveWith)} 
-                      className={getTabClass(Tab.SaveWith)}
-                  >
-                      <span>Save with&nbsp;2.10%*</span>
-                  </li>
-                  <li 
-                      onClick={()=> setCurrentTab(Tab.GlobalETFPortfolios)} 
-                      className={getTabClass(Tab.GlobalETFPortfolios)}>
-                       <span>Global ETF Portfolios</span>
-                  </li>
-                  <li 
-                      onClick={()=> setCurrentTab(Tab.GreatMortgageRates)}
-                      className={getTabClass(Tab.GreatMortgageRates)}
-                  >
-                      <span>Great Mortgage rates </span>
-                  </li>
-           </ul>    
-    </BannerBox>
+			<ul className={getTabsClass()}>
+				<li
+					onClick={() =>
+						setCurrentTab(Tab.SaveWith)
+					}
+					className={getTabClass(Tab.SaveWith)}
+				>
+					<span>Save with&nbsp;2.10%*</span>
+				</li>
+				<li
+					onClick={() =>
+						setCurrentTab(
+							Tab.GlobalETFPortfolios
+						)
+					}
+					className={getTabClass(
+						Tab.GlobalETFPortfolios
+					)}
+				>
+					<span>Global ETF Portfolios</span>
+				</li>
+				<li
+					onClick={() =>
+						setCurrentTab(
+							Tab.GreatMortgageRates
+						)
+					}
+					className={getTabClass(
+						Tab.GreatMortgageRates
+					)}
+				>
+					<span>Great Mortgage rates </span>
+				</li>
+			</ul>
+		</BannerBox>
+	)
 }
